5139cd24040e955d453caff77b523365f19d747a
Daniel <DanielP.SoftwareDev@gmail.com>
2024-10-04 12:16:21 -0400

    gitignore update

--- vite.config.ts
+++ vite.config.ts
@@ -8,12 +8,12 @@ export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: {
-      'assets': path.resolve(__dirname, 'src/assets'),
-      'components': path.resolve(__dirname, 'src/components'),
-      'contexts': path.resolve(__dirname, 'src/contexts'),
-      'helpers': path.resolve(__dirname, 'src/helpers'),
-      'hooks': path.resolve(__dirname, 'src/hooks'),
-      'views': path.resolve(__dirname, 'src/views'),
+      assets: path.resolve(__dirname, 'src/assets'),
+      components: path.resolve(__dirname, 'src/components'),
+      contexts: path.resolve(__dirname, 'src/contexts'),
+      helpers: path.resolve(__dirname, 'src/helpers'),
+      hooks: path.resolve(__dirname, 'src/hooks'),
+      views: path.resolve(__dirname, 'src/views'),
     },
   },
-})
+});
